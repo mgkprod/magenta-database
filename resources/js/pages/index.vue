4636c3a77e60c3adfa1638338454e724d230fa34
Simon Rubuano <sr@mgk.dev>
2020-12-15 21:12:25 +0100

    ✏️ "obectif"

--- resources/js/pages/index.vue
+++ resources/js/pages/index.vue
@@ -7,7 +7,7 @@
 
             <p class="mb-8">
                 Ce site est une base de données communautaire qui référence la plupart des tracks du groupe.<br>
-                L'obectif, à terme, est de garder une trace des différentes réinterprétations musicales et des tracks non publiées.<br>
+                L'objectif est de garder une trace des différentes réinterprétations musicales et des tracks non publiées.<br>
                 <br>
                 Le site est alimenté par les contributions des membres du Discord. N'hésitez pas à <a href="https://bit.ly/magentadiscord" target="_blank" class="transition duration-200 ease-in-out text-gray-default hover:text-gray-lighter">nous rejoindre</a> !<br>
             </p>
