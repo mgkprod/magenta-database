fbdab0d4621e2aaa686f954a48b9e032a917fc33
Simon Rubuano <sr@mgk.dev>
2020-12-19 18:40:43 +0100

    🔧 Configure css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,11 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: {
-        // enabled: true,
+        enabled: true,
         content: [
-            "app/**/*.php",
-            "resources/**/*.html",
             "resources/**/*.js",
-            "resources/**/*.jsx",
-            "resources/**/*.ts",
-            "resources/**/*.tsx",
             "resources/**/*.php",
-            "resources/**/*.vue",
-            "resources/**/*.twig",
+            "resources/**/*.vue"
         ],
         options: {
             // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
